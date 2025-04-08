2fa5182ffa838d085ac28bd3d2928e6a64af4dcf
AniketAinapur <aniket.ainapur23@vit.edu>
2025-04-09 00:10:28 +0530

    feat: initialize redux toolkit todo application with basic structure and components
    - Added package.json with dependencies for React, Redux Toolkit, Tailwind CSS, and Vite.
    - Created main application structure including App component, AddTodo and Todos components.
    - Implemented Redux store and todo slice for managing todo state.
    - Added form handling in AddTodo component for adding new todos.
    - Implemented Todos component to display and remove todos.
    - Integrated Tailwind CSS for styling.
    - Set up Vite configuration for the project.

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,31 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+    todos:[{id:1,text:"Hello"}]
+}
+
+export const todoSlice = createSlice({
+    name:"todo",
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const todo = {
+                id : nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos = state.todos.filter((todo)=>(todo.id !== action.payload))
+        },
+        updateTodo:(state,action)=>{
+            
+        }
+    }
+    
+})
+
+export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+
+export default todoSlice.reducer
--- todoContextLocalStorage/src/components/TodoForm.jsx
+++ todoContextLocalStorage/src/components/TodoForm.jsx
@@ -12,18 +12,20 @@ function TodoForm() {
 
         addTodo({todo,completed:false})
         setTodo("")
+        document.getElementById('myForm').reset()
+        
     }
 
      
     return (
-        <form onSubmit={add} className="flex">
+        <form onSubmit={add} className="flex" id='myForm'>
             <input
                 type="text"
                 placeholder="Write Todo..."
                 onChange={(e) => (setTodo(e.target.value))}
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
             />
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+            <button type="submit" id="myInput" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
             </button>
         </form>
